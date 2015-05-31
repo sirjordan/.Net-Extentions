5f49b6781fb76ccdd7492744880945aebff77117
Martin Marinov <sirjordan1988@gmail.com>
2015-05-31 14:39:59 +0300

    Project created. Added some code.

---
+++ DotNetExtentions/Extentions.Common/ObjectExtender.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Extentions.Common
+{
+    public static class ObjectExtender
+    {
+        /// <summary>
+        /// Copy the equal property values from original object to receiver.
+        /// Copy only value type properties (and string) with same name and type.
+        /// You can use different object types.
+        /// </summary>
+        /// <param name="original">Source</param>
+        /// <param name="receiver">Destination</param>
+        public static void MapProperties(this object original, object receiver)
+        {
+            List<PropertyInfo> originalProps = original.GetType().GetProperties().ToList();
+            List<PropertyInfo> receiverProps = receiver.GetType().GetProperties().ToList();
+
+            foreach (PropertyInfo prop in originalProps)
+            {
+                if (receiverProps.Exists(p => p.Name == prop.Name
+                    && p.PropertyType.IsAssignableFrom(prop.PropertyType))
+                    && (prop.PropertyType.IsValueType || prop.PropertyType == typeof(string)))
+                {
+                    string name = prop.Name;
+                    object value = original.GetType().GetProperty(name).GetValue(original, null);
+                    receiver.GetType().GetProperty(name).SetValue(receiver, value, null);
+                }
+            }
+        }
+
+        public static void CopyPropertiesTo<T>(this T source, T destination)
+        {
+            PropertyInfo[] destinationProperties = destination.GetType().GetProperties();
+            foreach (PropertyInfo destinationProperty in destinationProperties)
+            {
+                if (destinationProperty.CanWrite)
+                {
+                    PropertyInfo sourceProperty = source.GetType().GetProperty(destinationProperty.Name);
+                    if (sourceProperty.PropertyType.IsValueType || sourceProperty.PropertyType == typeof(string))
+                    {
+                        destinationProperty.SetValue(destination, sourceProperty.GetValue(source, null), null);
+                    }
+                }
+            }
+        }
+
+        public static string ToStringOrNull(this object o)
+        {
+            if (o == null)
+            {
+                return null;
+            }
+            else
+            {
+                return o.ToString();
+            }
+        }
+
+        public static int? ToIntOrNull(this object o)
+        {
+            if (o == null)
+            {
+                return null;
+            }
+            else
+            {
+                int value;
+                if (int.TryParse(o.ToString(), out value))
+                {
+                    return value;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static decimal? ToDecimalOrNull(this object o)
+        {
+            if (o == null)
+            {
+                return null;
+            }
+            else
+            {
+                decimal value;
+                if (decimal.TryParse(o.ToString(), out value))
+                {
+                    return value;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        public static DateTime? ToDateOrNull(this object o)
+        {
+            if (o == null)
+            {
+                return null;
+            }
+            else
+            {
+                DateTime value;
+                if (DateTime.TryParse(o.ToString(), out value))
+                {
+                    return value;
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+    }
+}
---
+++ DotNetExtentions/Extentions.WebForms/ControlExtender.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI;
+
+namespace Extentions.WebForms
+{
+    public static class ControlExtender
+    {
+        /// <summary>
+        /// Recursively finds controls of Type T, started from the root.
+        /// </summary>
+        /// <typeparam name="T">Type of the returned controls</typeparam>
+        /// <param name="root">Root control to starts from</param>
+        public static IEnumerable<T> GetControlsRecursive<T>(this Control root) where T : Control
+        {
+            foreach (T control in root.Controls.OfType<T>())
+            {
+                yield return (T)control;
+
+                if (control.HasControls())
+                {
+                    foreach (T childControl in GetControls<T>(control))
+                    {
+                        yield return childControl;
+                    }
+                }
+            }
+        }
+
+        /// <summary>
+        /// Finds controls of Type T.
+        /// </summary>
+        /// <typeparam name="T">Type of the returned controls</typeparam>
+        /// <param name="controlToSearch">Control to search</param>
+        public static IEnumerable<T> GetControls<T>(this Control controlToSearch) where T : Control
+        {
+            foreach (T child in controlToSearch.Controls.OfType<T>())
+            {
+                yield return (T)child;
+            }
+        }
+
+        /// <summary>
+        /// Find a control from 'root' recursive with that has id 'controlID'.
+        /// </summary>
+        /// <param name="root">Control to starts from</param>
+        /// <param name="controlID">ID of the control to find</param>
+        /// <returns>Found control or null if not found</returns>
+        public static Control FindControlRecursive(this Control root, string controlID)
+        {
+            if (!root.HasControls())
+            {
+                return null;
+            }
+
+            Control ctrl = root.FindControl(controlID);
+            if (ctrl != null)
+            {
+                return ctrl;
+            }
+            else
+            {
+                return root.FindControlRecursive(controlID);
+            }
+        }
+    }
+}
