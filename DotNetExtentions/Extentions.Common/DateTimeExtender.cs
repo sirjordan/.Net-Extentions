04a754ab9088351a9c0bcc2a2258e3409e224520
Martin Marinov <sirjordan1988@gmail.com>
2015-05-31 14:52:48 +0300

    DateTimeExtender Added. Test project added. Some code added.

---
+++ DotNetExtentions/Extentions.Common/DateTimeExtender.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extentions.Common
+{
+    public static class DateTimeExtender
+    {
+        public static DateTime GetDateForDayOfWeek(this DateTime dateInThisWeek, DayOfWeek dayOfWeek)
+        {
+            DateTime monday = dateInThisWeek.AddDays(-((int)dateInThisWeek.DayOfWeek - 1));
+            DateTime result;
+
+            if (dayOfWeek == DayOfWeek.Sunday)
+            {
+                result = monday.AddDays(6);
+            }
+            else
+            {
+                result = monday.AddDays((int)dayOfWeek - 1);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ DotNetExtentions/Extentions.Tests/TestsDateTimeExtender.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Extentions.Common;
+
+namespace Extentions.Tests
+{
+    [TestClass]
+    public class TestsDateTimeExtender
+    {
+        [TestMethod]
+        public void GetDateForDayOfWeekShoulReturnProperDates()
+        {
+            DateTime wednestday = new DateTime(2015, 5, 27);
+            DateTime monday = new DateTime(2015, 5, 25);
+            DateTime tuesday = new DateTime(2015, 5, 26);
+            DateTime sunday = new DateTime(2015, 5, 31);
+
+            DateTime mondayAsResult = wednestday.GetDateForDayOfWeek(DayOfWeek.Monday);
+            DateTime tuestdayAsResult = wednestday.GetDateForDayOfWeek(DayOfWeek.Tuesday);
+            DateTime sundayAsResult = wednestday.GetDateForDayOfWeek(DayOfWeek.Sunday);
+            
+            Assert.AreEqual(monday, mondayAsResult);
+            Assert.AreEqual(tuesday, tuestdayAsResult);
+            Assert.AreEqual(sunday, sundayAsResult);
+        }
+    }
+}
